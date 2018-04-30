3e8320a608eb52ebcc4417d82c88f32282aeda63
Kelsey Fecho <kfecho30@gmail.com>
2018-04-30 18:24:35 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,10 @@ class Post < ActiveRecord::Base
   validate :content, length: {minimum: 250}
   validate :summary, length: {maximum: 250}
   validate :category, inclusion: {in: %w(Fiction Non-Fiction)}
-  validate
+  validate :clickbait?
+
+  private
+
+  def clickbait 
+  end
 end
